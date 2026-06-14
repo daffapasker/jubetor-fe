9771a343567f4811bf7ad27329e5b4e117bb25ca
fridoa <fridoafriyanto3@gmail.com>
2026-06-15 04:26:27 +0700

    uodate Login Page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,10 @@
-
 import LoginForm from "@/components/views/LoginForm";
+import { Suspense } from "react";
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginForm />
+    </Suspense>
+  );
 }
