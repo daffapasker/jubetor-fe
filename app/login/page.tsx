ebd4d4220191db6d409a89a530ed79d1c247aff8
fridoa <fridoafriyanto3@gmail.com>
2026-06-15 04:31:12 +0700

    feat: create login page with LoginForm component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,9 +2,6 @@ import LoginForm from "@/components/views/LoginForm";
 import { Suspense } from "react";
 
 export default function LoginPage() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <LoginForm />
-    </Suspense>
-  );
+  return <LoginForm />
+
 }
